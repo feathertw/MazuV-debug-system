eafc4108fe605887075eda029300acd606fc85ec
fea.wang <feathertw@gmail.com>
2019-09-24 17:21:46 +0800

    Add dm header and halt and resume function

--- rtl/dm.v
+++ rtl/dm.v
@@ -1,7 +1,7 @@
 module dm #(
 	parameter NULL = 1
 ) (
-        output reg        interrupt,
+        output            interrupt,
         input             dmi_valid,
         output reg        dmi_ready,
         input             dmi_write,
@@ -19,46 +19,71 @@ module dm #(
 	input resetn,
 	input clk
 );
-        reg [31:0] register [0:7];
-        reg [31:0] ram [0:7];
 
-        reg [7:0] rom_file [0:128*4-1];
-        wire [31:0] rom [0:128-1];
+        `include "debug/rtl/dm_header.v"
 
-        genvar i;
-        generate
-                for (i = 0; i < 128; i = i + 1) begin
-                        assign rom[i] = {rom_file[i*4+3],rom_file[i*4+2],rom_file[i*4+1],rom_file[i*4+0]};
-                end
-        endgenerate
+        integer i;
+        localparam ROM_SIZE = 'h200;
+
+
+        reg  [ 7:0] rom_file [0:ROM_SIZE-1];
+        wire [31:0] rom      [0:ROM_SIZE/4-1];
         initial begin
                 $readmemh("debug/src/dm_rom.hex", rom_file);
         end
+        genvar gi;
+        generate
+                for (gi = 0; gi < ROM_SIZE; gi = gi + 4) begin
+                        assign rom[gi/4] = {rom_file[gi+3],rom_file[gi+2],rom_file[gi+1],rom_file[gi+0]};
+                end
+        endgenerate
 
         wire dmi_match = dmi_valid && dmi_ready;
+        assign interrupt = dmcontrol_haltreq ;
 
+        reg [31:0] dmi_register [0:2**7-1];
+        always @* begin
+                for (i=0; i<2**7; i=i+1) begin
+                        dmi_register[i] = 32'h0;
+                end
+                dmi_register[DMI_ADDR_DMCONTROL][`DMCONTROL_HALTREQ_RANGE]  = dmcontrol_haltreq;
+                dmi_register[DMI_ADDR_DMCONTROL][`DMCONTROL_DMACTIVE_RANGE] = dmcontrol_dmactive;
+        end
+
+
+        reg [`DMCONTROL_HALTREQ_RANGE]  dmcontrol_haltreq;
+        reg [`DMCONTROL_DMACTIVE_RANGE] dmcontrol_dmactive;
+        always @(posedge clk) begin if(!resetn) begin
+                        dmcontrol_dmactive <= `DMCONTROL_DMACTIVE_WIDTH'h0;
+                end else if(dmi_match && dmi_write && dmi_addr==DMI_ADDR_DMCONTROL) begin
+                        dmcontrol_dmactive <= dmi_wdata[`DMCONTROL_DMACTIVE_RANGE];
+                end
+        end
         always @(posedge clk) begin
-                if(dmi_match && dmi_write) begin
-                        register[dmi_addr] <= dmi_wdata;
+                if(!resetn || !dmcontrol_dmactive) begin
+                        dmcontrol_haltreq <= `DMCONTROL_HALTREQ_WIDTH'h0;
+                end else if(dmi_match && dmi_write && dmi_addr==DMI_ADDR_DMCONTROL) begin
+                        dmcontrol_haltreq <= dmi_wdata[`DMCONTROL_HALTREQ_RANGE];
                 end
         end
 
-        always @(posedge clk) begin
-                if(!resetn) begin
-                        interrupt <= 0;
-                end else if(dmi_match && dmi_write) begin
-                        interrupt <= 1;
-                end else if (bus_match && bus_write && {bus_addr,2'h0}=='h400)begin
-                        interrupt <= 0;
-                end
-        end
+        // dm_request: [31]==valid, [30:20]==request, [19:0]==hartid
+        // request==0 resume
+        // request==1 set_s0
+        // request==2 set_s1
+        // request==3 set_other_gpr
+        // request==4 get_s0
+        // request==5 get_s1
+        // request==6 get_other_gpr
+        // request==7 set_csr
+        // request==8 get_csr
         reg [31:0] dm_request;
         always @(posedge clk) begin
                 if(!resetn) begin
                         dm_request <= 0;
-                end else if(dmi_match && !dmi_write) begin
+                end else if(dmi_match && dmi_write && dmi_addr==DMI_ADDR_DMCONTROL && dmi_wdata[`DMCONTROL_RESUMEREQ_RANGE]) begin
                         dm_request <= 32'h8000_0000;
-                end else if (bus_match && bus_write && {bus_addr,2'h0}=='h420)begin
+                end else if (bus_match && bus_write && {bus_addr,2'h0}==BUS_ADDR_DM_REQUEST)begin
                         dm_request <= 0;
                 end
         end
@@ -68,7 +93,7 @@ module dm #(
                 if(!resetn) begin
                         dmi_rdata <= 0;
                 end else if (dmi_valid && ~dmi_write) begin
-                        dmi_rdata <= register[dmi_addr];
+                        //dmi_rdata <= register[dmi_addr];
                 end
         end
 
@@ -84,16 +109,6 @@ module dm #(
 
         wire bus_match = bus_valid && bus_ready;
 
-        always @(posedge clk) begin
-                if(!resetn) begin
-                        ram[0] <= 32'h00000013;
-                        ram[1] <= 32'h00000013;
-                        ram[2] <= 32'h0400000b;
-                        ram[3] <= 32'h0000006f;
-                end else if(bus_match && bus_write) begin
-                        ram[bus_addr] <= bus_wdata;
-                end
-        end
 
         always @(posedge clk) begin
                 if(!resetn) begin
@@ -101,7 +116,7 @@ module dm #(
                 end else if (bus_valid && !bus_write) begin
                         if({bus_addr,2'h0}<'h200) begin
                                 bus_rdata <= rom[bus_addr];
-                        end else if({bus_addr,2'h0} == 'h420) begin
+                        end else if({bus_addr,2'h0} == BUS_ADDR_DM_REQUEST) begin
                                 bus_rdata <= dm_request;
                         end
                 end
