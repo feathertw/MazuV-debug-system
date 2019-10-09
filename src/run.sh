03e357d4e012151b1bd7f190c19f7fa419c55018
fea.wang <feathertw@gmail.com>
2019-10-09 13:28:14 +0800

    Rename assembly to uppercase .S for sometimes need preprocessor

--- src/run.sh
+++ src/run.sh
@@ -1,3 +1,3 @@
-riscv32-unknown-elf-gcc -nostdlib dm_rom.s -Ttext=0x0 -Tdata=0x200 -e0x0 -o /tmp/a.out
+riscv32-unknown-elf-gcc -nostdlib dm_rom.S -Ttext=0x0 -Tdata=0x200 -e0x0 -o /tmp/a.out
 riscv32-unknown-elf-objdump -d /tmp/a.out > dm_rom.lst
 riscv32-unknown-elf-objcopy -O verilog /tmp/a.out dm_rom.hex
